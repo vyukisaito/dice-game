94077fae366b082206b921445b7ff8018b71e91b
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-06-20 23:09:07 -0300

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+const score0El = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice')
+
+score0El.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add('hidden')
